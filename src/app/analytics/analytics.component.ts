92cb4e6398c7ed83d3c48f232f96480786352022
Abou Zuhayr <abouzuhayr@Abous-MacBook-Air.local>
2021-11-17 15:22:26 +0530

    analytics the first chart was added

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
+import { singles } from './dumy_data';
 
 @Component({
   selector: 'app-analytics',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsComponent implements OnInit {
 
-  constructor() { }
+  singles!: any[];
+  view: [number, number] = [1000, 500];
+
+  // options
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+
+  colorScheme = {
+    name: 'night',
+    selectable: false,
+    group: ScaleType.Linear,
+    domain: [
+      '#2B1B5A',
+      '#501356',
+      '#183356',
+      '#28203F',
+      '#391B3C',
+      '#1E2B3C',
+      '#120634',
+      '#2D0432',
+      '#051932',
+      '#453080',
+      '#75267D',
+      '#2C507D',
+      '#4B3880',
+      '#752F7D',
+      '#35547D'
+    ]
+  }
+
+  constructor() {
+    Object.assign(this, { singles });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSelect(event: any) {
+    console.log(event);
+  }
+
+  onResize(event: any) {
+    this.view = [event.target.innerWidth / 1.35, 400];
   }
 
 }
