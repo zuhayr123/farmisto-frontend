76801b31ac03d57b480facc73ae1dc2073fe2b99
Abou Zuhayr <abouzuhayr@Abous-MacBook-Air-2.local>
2022-02-06 02:42:36 +0530

    feature test to be ready tomorrow

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -53,18 +53,55 @@ export class CoursesComponent implements AfterViewInit {
     });
   }
 
-  openBulkCourseUpload(row : any) {
+  openBulkCourseUpload(row: any) {
     console.log(JSON.stringify(row));
+    console.log("******************* NEW DATA ********************")
+    this.replaceIds(row);
+
     const dialogRef = this.dialog.open(DialogCourseBulkCreateComponent);
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
       if (result == true) {
         // this.deleteJobs(id);
       }
     });
   }
 
+  replaceIds(row: any) {
+    Array.from(row.children).forEach(chapter => {
+      delete (chapter as any)._id
+      Array.from((chapter as any).children).forEach(subchapter => {
+        delete (subchapter as any)._id
+        Array.from((subchapter as any).children).forEach(content => {
+          delete (content as any)._id
+        })
+      })
+    });
+
+    var dao = row as CourseContentTreeModel
+    var newstr = JSON.stringify(row).replace(row.course_name, row.course_name + " copy 01");
+    var data = this.replaceAll(newstr, row.course_id , Date.now().toString());
+    delete row._id
+
+    dao.children.forEach(chapter =>{
+      data = this.replaceAll(data, chapter.content_id, (Date.now()+1).toString());
+      chapter.children.forEach(subchapter => {
+        data = this.replaceAll(data, subchapter.content_id, (Date.now()+5).toString());
+        subchapter.children.forEach(content => {
+          data = this.replaceAll(data, content.content_id, (Date.now()+10).toString());
+        })
+      })
+    });
+    console.log(JSON.stringify(dao));
+  }
+
+  escapeRegExp(string: String) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+  }
+  
+  replaceAll(str: string, find: string, replace: string) {
+    return str.replace(new RegExp(this.escapeRegExp(find), 'g'), replace);
+  }
   getCourseList() {
     this.service.getCourseList().subscribe((res) => {
       console.log("the resoponse received was " + JSON.stringify(res))
@@ -75,7 +112,7 @@ export class CoursesComponent implements AfterViewInit {
     });
   }
 
-  showCourseDetails(row:any) {
+  showCourseDetails(row: any) {
     console.log("the data visible in the course component while navigation is " + JSON.stringify(row));
     this.service.courseContentTreeModel = row as CourseContentTreeModel;
     this.router.navigateByUrl("ccp/add_course");
